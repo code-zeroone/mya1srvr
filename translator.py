31c765c367a763b25a2011e68e167f761fe3af30
Code-01 <64568214+code-zeroone@users.noreply.github.com>
2025-12-26 00:02:13 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,1 +1,11 @@
-0
+from deep_translator import GoogleTranslator
+
+class Translator:
+    def __init__(self, cfg):
+        self.enabled = cfg["translate"]
+
+    def translate(self, text, src, target):
+        try:
+            return GoogleTranslator(source=src, target=target).translate(text)
+        except:
+            return text
