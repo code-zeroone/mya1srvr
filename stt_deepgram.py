7e9d2f9358a4bf59d9ba5a7d0035f68a6bffeebb
Code-01 <64568214+code-zeroone@users.noreply.github.com>
2025-12-26 00:00:55 +0200

    Update stt_deepgram.py

--- stt_deepgram.py
+++ stt_deepgram.py
@@ -1,36 +1,29 @@
-import asyncio
-import ssl
-import json
-import websockets
+import asyncio, websockets, ssl, json
 
-class DeepgramClient:
-
-    def __init__(self):
-        self.queue = asyncio.Queue()
+class DeepgramSTT:
+    def __init__(self, cfg):
+        self.q = asyncio.Queue()
         self.loop = asyncio.get_event_loop()
-        self.loop.create_task(self._connect())
+        self.loop.create_task(self._connect(cfg["deepgram_key"]))
 
-    async def _connect(self):
+    async def _connect(self, key):
         ssl_ctx = ssl.create_default_context()
         self.ws = await websockets.connect(
             "wss://api.deepgram.com/v1/listen?"
-            "encoding=linear16&sample_rate=16000&channels=1&language=ar",
-            headers=[("Authorization", "Token YOUR_API_KEY")],
+            "encoding=linear16&sample_rate=16000&channels=1",
+            headers=[("Authorization", f"Token {key}")],
             ssl=ssl_ctx
         )
 
-        async def recv():
-            async for msg in self.ws:
-                data = json.loads(msg)
-                if "channel" in data:
-                    txt = data["channel"]["alternatives"][0]["transcript"]
-                    if txt:
-                        await self.queue.put(txt)
+        async for msg in self.ws:
+            data = json.loads(msg)
+            if "channel" in data:
+                alt = data["channel"]["alternatives"][0]
+                if alt["transcript"]:
+                    await self.q.put((alt["transcript"], alt.get("language","auto")))
 
-        self.loop.create_task(recv())
-
-    def send_audio(self, data):
-        asyncio.run_coroutine_threadsafe(self.ws.send(data), self.loop)
+    async def send_audio(self, data):
+        await self.ws.send(data)
 
     async def get_text(self):
-        return await self.queue.get()
+        return await self.q.get()
